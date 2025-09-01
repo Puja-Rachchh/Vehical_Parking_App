33eff457089f74236259bab5dbec388404efb9f7
Puja Rachchh <143953428+Puja-Rachchh@users.noreply.github.com>
2025-09-01 18:01:33 +0530

    Refactor app.py to streamline initialization process
    Refactor app initialization and admin user creation logic.

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
-# app.py
 from config import create_app, db, login_manager
-from backend.models import User, ParkingLot, ParkingSpot, ReserveParking
+from backend.models import User
 
 app = create_app()
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+with app.app_context():
+    from backend.routes import init_routes
+    init_routes(app)
+    db.create_all()
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
+        print("Creating admin user...")
+        admin = User(username='admin', email='admin@example.com',
+                     full_name='Admin User', is_admin=True)
+        admin.set_password('admin')
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin user created successfully")
+    else:
+        print(f"Admin user exists. Admin status: {admin.is_admin}")
 
-#import routes  # Import routes to register them with the app
-if __name__ == '__main__':
-    with app.app_context():
-        from backend.routes import init_routes
-        init_routes(app)
-        db.create_all()
-        admin = User.query.filter_by(username='admin').first()
-        if not admin:
-            print("Creating admin user...")
-            admin = User(username='admin', email='admin@example.com',full_name='Admin User', is_admin=True)
-            admin.set_password('admin')
-            db.session.add(admin)
-            db.session.commit()
-            print("Admin user created successfully")
-        else:
-            print(f"Admin  user exists. Admin status: {admin.is_admin}")
-    app.run(debug=True)
+# Remove app.run(debug=True)
